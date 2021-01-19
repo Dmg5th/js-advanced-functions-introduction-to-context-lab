fc45502b4b6411db8cc5b781e108c64ebc6ee2df
Dmg5th <dmgould310@gmail.com>
2021-01-19 09:55:15 -0500

    commit tree d61385de69de0f84ab0589b2470bb3c4e08bd2c1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// let arr = ["Chris Multisanti", "Tony Soprano", "Silvio Dante", "Paulie Walnuts", "Bobby Baccala"]
+
+
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array){
+    return array.map(arr => createEmployeeRecord(arr))
+}
+
+
+function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date,
+    })
+
+    return employee
+}
